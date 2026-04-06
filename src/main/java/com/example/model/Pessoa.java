8d83a1e918e28719878f628b633ba07efae4b375
CamilaDembogurski <camilamdembogurski@gmail.com>
2026-04-06 20:21:20 -0300

    segundo commit

---
+++ src/main/java/com/example/model/Pessoa.java
@@ -0,0 +1,97 @@
+package com.example.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Pessoa {
+    private Integer id;
+    private String nome;
+    private String sobrenome;
+    private Date dataNascimento;
+    private String cpf;
+    private Endereco endereco;
+
+    public Pessoa(Integer id, String nome, String sobrenome, Date dataNascimento, String cpf, Endereco endereco) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Pessoa(String nome, String sobrenome, Date dataNascimento, String cpf, Endereco endereco) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.endereco = endereco;
+    }
+
+    public Pessoa(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Pessoa() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", sobrenome='" + sobrenome + '\'' +
+                ", dataNascimento=" + dataNascimento +
+                ", cpf='" + cpf + '\'' +
+                ", endereco=" + endereco +
+                '}';
+    }
+}
